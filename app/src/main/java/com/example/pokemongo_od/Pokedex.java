4c88ebf7074c29a31ff03516e895deda4ca74952
Gawesomer <jj.jouband@gmail.com>
2019-11-18 18:47:21 -0600

    Database only gets copied to internal storage if database does not already exist.

--- app/src/main/java/com/example/pokemongo_od/Pokedex.java
+++ app/src/main/java/com/example/pokemongo_od/Pokedex.java
@@ -1,16 +1,12 @@
 package com.example.pokemongo_od;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 public class Pokedex {
@@ -25,36 +21,42 @@ public class Pokedex {
         for (int i = 0; i < pokemonList.length; i++) {
             pokemonList[i] = new Pokemon(context, i+1);
         }
-        dbHelper = new DBHelper(context);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        // Create a new map of values, where column names are the keys
-        ContentValues values = new ContentValues();
-        values.put(DBContract.PokedexDB.POKEMON_NAME, pokemonList[0].getName());
-        values.put(DBContract.PokedexDB.CATCH_STATE, pokemonList[0].getCatchState());
-        // Insert the new row, returning the primary key value of the new row
-        long newRowId = db.insert(DBContract.PokedexDB.TABLE_NAME, null, values);
+        if (!databaseExists(context, DBHelper.DATABASE_NAME)) {
+            dbHelper = new DBHelper(context);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            try {
+                InputStream dbAssets = context.getAssets().open("Pokedex.db");
+                String dbPathName = context.getApplicationInfo().dataDir + "/databases/" + DBHelper.DATABASE_NAME;
+                copyFile(context, dbAssets, dbPathName);
+            } catch (IOException e) {
+                Log.e("Exception: %s", e.getMessage());
+            }
+        }
+    }
 
-        InputStream is = null;
-        OutputStream os = null;
+    // Checks if database exists
+    private boolean databaseExists(Context context, String dbName) {
+        File dbFile = context.getDatabasePath(dbName);
+        return dbFile.exists();
+    }
+
+    // Used to copy database from assets to internal storage
+    private void copyFile(Context context, InputStream toCopy, String destPath) {
+        OutputStream outputStream;
         try {
-            is = context.getAssets().open("Pokedex.db");
-            String file = "/data/data/com.example.pokemongo_od/databases/Pokedex.db";
-            os = new FileOutputStream(file);
+            outputStream = new FileOutputStream(destPath);
 
             byte[] buffer = new byte[1024];
             int length = 0;
-            while ((length = is.read(buffer)) > 0) {
-                os.write(buffer, 0, length);
+            while ((length = toCopy.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
             }
-            os.flush();
-            is.close();
-            os.close();
+            outputStream.flush();
+            toCopy.close();
+            outputStream.close();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-
     }
 
     public static Pokedex getInstance(Context context) {
